d67b36591244088eeac78aabee1f770a677f99ff
Gjermund Skobba <gjermund@skobba.net>
2021-02-20 13:31:02 +0100

    Selecting Redux and remove the 2 other

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import ReduxComponent from './view/reduxComponent.jsx';
-import ReactComponent from './view/component.jsx';
-import ReactHooksComponent from './view/hooksComponent.jsx';
 import store from './redux/store';
-import { Provider } from 'react-redux'
 
 export default class App extends Component {
-
   render() {
     return (
       <>
         <h2>Sopra Steria Kalkulator!</h2>
-        <ReactComponent/>
-        {/*<ReactHooksComponent />*/}
-        {/*<Provider store={store}>
-          <ReduxComponent/>
-        </Provider>*/}
+        <Provider store={store}>
+          <ReduxComponent />
+        </Provider>
       </>
     );
   }
